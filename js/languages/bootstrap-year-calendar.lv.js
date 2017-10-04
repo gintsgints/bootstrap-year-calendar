95fd3d6f3a6f21f3b0cff12bb70603da28625faf
Gints Polis <gints.polis@lattelecom.lv>
2017-10-04 09:00:40 +0300

    Latvian language support

---
+++ js/languages/bootstrap-year-calendar.lv.js
@@ -0,0 +1,18 @@
+/**
+ * Latvian translation for bootstrap-year-calendar
+ * Gints Polis
+ * <polis.gints@gmail.com>
+ */
+
+;
+(function($) {
+  $.fn.calendar.dates['lv'] = {
+    days: ["Svētdiena", "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena"],
+    daysShort: ["Sve", "Prd", "Otr", "Tre", "Cet", "Pkd", "Ses"],
+    daysMin: ["Sv", "Pr", "Ot", "Tr", "Ce", "Pt", "Se"],
+    months: ["Janvāris", "Februāris", "Marts", "Aprīlis", "Maijs", "Jūnijs", "Jūlijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Decembris"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+    weekShort: 'n',
+    weekStart: 1
+  };
+}(jQuery));
